d4e43fad017b12fde80501863cc0464de547db76
MilenaGkm <milenagaranin@gmail.com>
2021-07-15 16:20:28 +0300

    added tabs

---
+++ src/components/Msgs.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Msgs() {
+    return (
+        <div>
+            <div>༼ つ ◕_◕ ༽つ</div>
+            <div>Msgs Tab</div>
+        </div>
+    )
+}
